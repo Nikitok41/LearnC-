896d872309bb4ceb1da5af80cd334fcdc68e590e
Nikitok41 <nik777pups@gmail.com>
2022-04-23 18:16:52 +0300

    ПОКА ДАША!

---
+++ Example05/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string usrename = Console.ReadLine();
+
+if (usrename.ToLower() == "даша")
+{
+    Console.WriteLine("Не пиши мне!");
+}
+else 
+{
+    Console.Write("Привет, ");
+    Console.Write(usrename);
+}
